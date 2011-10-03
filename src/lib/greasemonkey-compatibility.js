0fb6ae5f09623ff14842ae7d45b4f6cd7c1e7b33
Jan Chimiak <jan.chimiak@gmail.com>
2011-10-03 08:12:31 +0200

    Removed ie non-support

--- src/lib/greasemonkey-compatibility.js
+++ src/lib/greasemonkey-compatibility.js
@@ -6,40 +6,14 @@
  * This document is licensed as free software under the terms of the
  * MIT License: http://www.opensource.org/licenses/mit-license.php
  */
-// ie support relies on user agent. if ua is spoofed, it will crash
-if ($.browser.msie) {
-	if ((typeof PRO_setValue) !== "undefined") {		
-		this.GM_getValue = function (key, def) {
-			return PRO_getValue(key) || def;
-		};
-		this.GM_setValue = function (key, value) {
-			return PRO_setValue(key, value);
-		};
-		this.GM_deleteValue = function (key) {
-			return PRO_deleteValue(key);
-		};
-		this.GM_listValues = function () {
-			return PRO_listValues();
-		};
-		this.GM_xmlhttpRequest = function (config) {
-			return PRO_xmlhttpRequest(config);
-		};
-		this.GM_openInTab = function (link) {
-			return PRO_openInTab(link);
-		};
-	}
-}
-//webkit (chrome) support relies on user agent. if ua is spoofed, it will crash
-else if ($.browser.webkit) {
-	if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported") > -1)) {
-		this.GM_getValue = function (key, def) {
-			return localStorage[key] || def;
-		};
-		this.GM_setValue = function (key, value) {
-			return (localStorage[key] = value);
-		};
-		this.GM_deleteValue = function (key) {
-			return delete localStorage[key];
-		};
-	}
+if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported") > -1)) {
+	this.GM_getValue = function (key, def) {
+		return localStorage[key] || def;
+	};
+	this.GM_setValue = function (key, value) {
+		return (localStorage[key] = value);
+	};
+	this.GM_deleteValue = function (key) {
+		return delete localStorage[key];
+	};
 }
